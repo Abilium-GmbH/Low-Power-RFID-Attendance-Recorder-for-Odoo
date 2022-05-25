e765891713a68d9a5bda1738d5d9d76d05626785
admintm <m@neville.ch>
2022-05-25 04:00:17 +0200

    both "too many files" bugs apparently fixed

--- src/main.py
+++ src/main.py
@@ -23,18 +23,24 @@ global key1Pressed
 key1Pressed = False
 global key2Pressed
 key2Pressed = False
+global keypresstimestamp 
+keypresstimestamp = 0
 
 def button1_pressed_callback(channel):
     global key1Pressed
-    print("Button 1 pressed!")
     key1Pressed = True
+    # needed as a timeout for key presses
+    global keypresstimestamp
+    keypresstimestamp = time.time()
 
 def button2_pressed_callback(channel):
     global key2Pressed
-    print("Button 2 pressed!")
     key2Pressed = True
+    # needed as a timeout for key presses
+    global keypresstimestamp
+    keypresstimestamp = time.time()
 
-
+#async. button management
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BUTTON1_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(BUTTON2_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -46,55 +52,65 @@ try:
   odoo_handler.getLogo()
   illustrator = Illustrator()
   illustrator.initialScreen()
+  reader = SimpleMFRC522()
 
   now = datetime.now()
   oldmin = int(now.strftime("%M"))
-  
+
   while(True):
-    # wait for a nfc token from an employee
-    reader = SimpleMFRC522()
     start = time.time()
+    #once current time is known check if keypresses have expired
+    if (keypresstimestamp + 5 < start) and (key1Pressed or key2Pressed):
+      keypresstimestamp = 0
+      key1Pressed = False
+      key2Pressed = False
+    #power up the RFID antenna
+    reader.READER.MFRC522_Init()
+    # listen for 0.25s for an RFID card
     id = reader.read_id_no_block()
     while (time.time() < start + 0.25) and (not id):
        id = reader.read_id_no_block()
-    #reader.READER.MFRC522_Reset()
+    #power down the RFID antenna
+    reader.READER.AntennaOff()
     if id:
-      
+      odoo_handler = Interpreter()
       try:
           employee = odoo_handler.getEmployee(id)
-          if key1Pressed: #key1.is_pressed:
-              #keyPressed = True
+          if key1Pressed:
               if employee.isCheckedOut:
                   illustrator.isCheckedInOutScreen(employee.name, True)
 
               if not employee.isCheckedOut:
                   illustrator.isCheckedInOutScreen(employee.name, False)
 
-          if employee.isCheckedOut and not key1Pressed and not key2Pressed:
+          if employee.isCheckedOut and not key1Pressed:
               odoo_handler.check_in(employee)
-              illustrator.checkInOutScreen(employee.name, str(timedelta(hours = employee.hours_this_month)).split(':')[0] + 'h  ' + str(timedelta(hours = employee.hours_this_month)).split(':')[1] + 'm', True)
-              #illustrator.checkInOutScreen(employee.name, str(timedelta(hours = employee.hours_this_month)).split(':')[0] + 'h  ' + str(timedelta(hours = employee.hours_this_month)).split(':')[1] + 'm', False)
-
-          if not employee.isCheckedOut and not key1Pressed and not key2Pressed:
-              odoo_handler.check_out(employee)
-              illustrator.checkInOutScreen(employee.name, str(timedelta(hours = employee.hours_this_month)).split(':')[0] + 'h  ' + str(timedelta(hours = employee.hours_this_month)).split(':')[1] + 'm', False)
               #illustrator.checkInOutScreen(employee.name, str(timedelta(hours = employee.hours_this_month)).split(':')[0] + 'h  ' + str(timedelta(hours = employee.hours_this_month)).split(':')[1] + 'm', True)
+              illustrator.checkInOutScreen(employee.name, str(timedelta(hours = employee.hours_this_month)).split(':')[0] + 'h  ' + str(timedelta(hours = employee.hours_this_month)).split(':')[1] + 'm', False)
+
+          if not employee.isCheckedOut and not key1Pressed:
+              odoo_handler.check_out(employee)
+              #illustrator.checkInOutScreen(employee.name, str(timedelta(hours = employee.hours_this_month)).split(':')[0] + 'h  ' + str(timedelta(hours = employee.hours_this_month)).split(':')[1] + 'm', False)
+              illustrator.checkInOutScreen(employee.name, str(timedelta(hours = employee.hours_this_month)).split(':')[0] + 'h  ' + str(timedelta(hours = employee.hours_this_month)).split(':')[1] + 'm', True)
 
       except ValueError:
             illustrator.unknownScreen()
 
-      if key2Pressed: #key2.is_pressed:
+      if key2Pressed:
           illustrator.checkIpAdress()
 
       time.sleep(5)
       illustrator.initialScreen()
+      #update minute change detector if a login/out already refreshed the clock on home
       now = datetime.now()
       newmin = int(now.strftime("%M"))
       oldmin = newmin
+      keypresstimestamp = 0
       key1Pressed = False
       key2Pressed = False
 
     else:
+      #minute change detector to refresh screen on minute change
       now = datetime.now()
       newmin = int(now.strftime("%M"))
       if not oldmin == newmin:
@@ -104,4 +120,5 @@ try:
         time.sleep(1)
 
 except KeyboardInterrupt:
+    reader.READER.Close_MFRC522()
     exit()
