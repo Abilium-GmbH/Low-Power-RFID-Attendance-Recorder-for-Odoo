e8e06c865ef8a9b271e05832ae03462ae00dd067
OxygenEnthusiast <narwal27@protonmail.com>
2022-05-30 21:47:10 +0200

    BUGFIX no more extra time on checkout

--- src/odoo/employee.py
+++ src/odoo/employee.py
@@ -12,7 +12,7 @@ class Employee():
     last_check_in: str
 
     def update_hours(self):
-        timediff = datetime.now() - datetime.strptime(self.last_check_in,"%Y-%m-%d %H:%M:%S")
+        timediff = datetime.utcnow() - datetime.strptime(self.last_check_in,"%Y-%m-%d %H:%M:%S")
         hoursdiff = round( timediff.total_seconds() / 3600) 
         self.hours_this_month += hoursdiff
         self.hours_today += hoursdiff
